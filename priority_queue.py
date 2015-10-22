f8c35f6697c540227e4e49c437b4c2e143f477f1
Jay LeCavalier <jay.lecavalier@colorado.edu>
2015-10-22 15:19:46 -0600

    Buggy push. Need to fix

--- priority_queue.py
+++ priority_queue.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 
 class Pq:
   def __init__(self,size):
@@ -7,8 +8,39 @@ class Pq:
   	  self.elts.append(float('inf'))
 
   def display(self):
-  	print("Current queue:")
+  	print("\nCurrent queue:")
   	print(self.elts)
+
+  def push(self,to_push):
+    if len(self.elts) < 1:
+      # Queue is empty, so just add the first element
+      self.elts = [to_push]
+    elif len(self.elts) == 1:
+      # Just one element in queue. 
+      # First element becomes the min of
+      # the first element and the item to push.
+      # Second element becomes other one.
+      even = min(self.elts[0],to_push)
+      odd = max(self.elts[0],to_push)
+      self.elts[0] = even
+      self.elts.append(odd)
+    else:
+      # First, update the first two elements
+      even = min(self.elts[0],to_push)
+      odd = max(self.elts[0],to_push)
+      if len(self.elts) == 2:
+        self.elts.append(self.elts[1])
+      self.elts[0] = even
+      self.elts[1] = odd
+      # Inductively update the rest of the queue
+      self.elts.append(float('inf'))
+      for i in range(1, int(math.ceil(len(self.elts)/2))-1):
+        even = min(self.elts[2*i],self.elts[(2*i)-1])
+        odd = max(self.elts[2*i],self.elts[(2*i)-1])
+        self.elts[2*i] = even
+        self.elts[(2*i)+1] = odd
+    # Everything okay!
+    print("\nDone")
 
 if __name__ == "__main__":
   argparser = argparse.ArgumentParser()
@@ -16,13 +48,24 @@ if __name__ == "__main__":
 
   args = argparser.parse_args()
 
-  # Initialize the queue and display its elements
+  # Initialize the queue
   pq = Pq(args.size)
-  pq.display()
 
+  # Give the user a friendly menu
   usr_choice = 0
   while usr_choice != 3:
-  	print("\n1: push something onto the queue")
-  	print("\n2: pop something from the queue")
-  	print("\n3: exit")
-  	print("\nWhat is your choice")
+    # Display the queue
+    pq.display()
+    # Give the user a friendly menu
+    print("\n1: push something onto the queue")
+    print("2: pop something from the queue")
+    print("3: exit")
+    usr_choice = input("\nEnter your choice: ")
+
+    # Push something
+    if usr_choice == 1:
+      pq.push(input("\nEnter the number you want to push: "))
+
+    # Pop something
+    elif usr_choice == 2:
+      print("2")
